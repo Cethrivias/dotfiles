0809119777fbdd2613cb3bf820ddedce533c454a
cethris <cethrivias@gmail.com>
2026-08-04 15:01:00 +0200

    [nvim] diagnostics signs

--- nvim/.config/nvim/lua/diagnostics.lua
+++ nvim/.config/nvim/lua/diagnostics.lua
@@ -4,7 +4,17 @@ vim.diagnostic.config({
     -- virtual_lines = { current_line = true },
     virtual_text = { current_line = true },
     underline = true,
+    signs = {
+        active = true,
+        text = {
+            [vim.diagnostic.severity.ERROR] = "⊗",
+            [vim.diagnostic.severity.WARN]  = "⚠",
+            [vim.diagnostic.severity.HINT]  = "",
+            [vim.diagnostic.severity.INFO]  = "",
+        },
+    },
 })
+local asd
 -- vim.diagnostic.config({ virtual_lines = { current_line = true } })
 
 -- Diagnostic keymaps
